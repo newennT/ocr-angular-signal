8c0987ed2391cee4fea8e88731439eeaa3ab4352
NewennT <newenn.turbiau@gmail.com>
2025-12-14 13:49:00 +0100

    signals simples

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   template: `
 		<div class="header-container">
-			<h1 class="header">Signalize</h1>
-			<p class="header-tagline">So your tasks aren't a chore to complete</p>
+			<h1 class="header">{{ header() }}</h1>
+			<p class="header-tagline">{{ tagline() }}</p>
 		</div>
   `,
   styles: ``
 })
 export class HeaderComponent {
+  private _taglines: string[] = [
+    `So your tasks aren't a chore to complete`,
+    `Organize your life with signals`,
+    `Task management made simple`,
+    `Stay productive with Signalize`
+];
+  header = signal('Signalize');
+  taglineIndex = signal(0);
+  tagline = computed(() => this._taglines[this.taglineIndex()]);
+
+  constructor() { 
+    setTimeout(() => this.header.set('Signalize !!'), 1000);
+    setTimeout(() => this.header.update(header => header + '??' ), 5000)
+    setInterval(() => this.taglineIndex.set(Math.floor(Math.random() * this._taglines.length)), 1000);
+
+  }
 
 }
