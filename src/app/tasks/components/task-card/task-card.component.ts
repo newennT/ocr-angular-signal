262b5f5ceb703d3a5f1e2e6980dd3963e1957669
NewennT <newenn.turbiau@gmail.com>
2025-12-16 15:39:29 +0100

    Chapitre 4 : input, output, model

--- src/app/tasks/components/task-card/task-card.component.ts
+++ src/app/tasks/components/task-card/task-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { TaskCardHeaderComponent } from './task-card-header/task-card-header.component';
 import { SubtasksListComponent } from './subtasks-list/subtasks-list.component';
 import { Task } from '../../models/task';
+import { TaskAction } from '../../models/task-action';
 
 @Component({
   selector: 'app-task-card',
@@ -11,8 +12,9 @@ import { Task } from '../../models/task';
   ],
   template: `
 		<div class="task-card" [class.completed]="completed()">
-			<app-task-card-header [task]="task()"/>
-      <app-subtasks-list [subtasks]="task().subtasks"/>
+			<app-task-card-header [task]="task()"
+														(taskAction)="onTaskAction($event)"/>
+			<app-subtasks-list [subtasks]="task().subtasks" (subtaskToggle)="onSubtaskToggle($event)"/>
 		</div>
   `,
   styleUrls: ['./task-card.component.scss']
@@ -20,4 +22,14 @@ import { Task } from '../../models/task';
 export class TaskCardComponent {
   task = input.required<Task>();
   completed = computed(() => this.task().completed);
+  taskAction = output<TaskAction>();
+  subtaskToggle = output<{ title: string; checked: boolean }>();
+
+  onTaskAction(action: TaskAction) {
+    this.taskAction.emit(action);
+  }
+
+  onSubtaskToggle(toggleEvent: { title: string; checked: boolean }) {
+    this.subtaskToggle.emit(toggleEvent);
+  }
 }
--- src/app/tasks/components/task-grid/task-grid.component.ts
+++ src/app/tasks/components/task-grid/task-grid.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
 import { NewTaskModalComponent } from '../new-task-modal/new-task-modal.component';
 import { TaskService } from '../../services/task.service';
+import { TaskAction } from '../../models/task-action';
 
 @Component({
   selector: 'app-task-grid',
@@ -12,7 +13,9 @@ import { TaskService } from '../../services/task.service';
   template: `
     <div class="task-grid">
 			@for (task of tasks(); track task.id) {
-        <app-task-card [task]="task"/>
+        <app-task-card [task]="task" 
+                       (taskAction)="onTaskAction(task.id, $event)" 
+                       (subtaskToggle)="onSubtaskToggle(task.id, $event)" />
 			}
     </div>
     <app-new-task-modal/>
@@ -35,4 +38,25 @@ import { TaskService } from '../../services/task.service';
 export class TaskGridComponent {
   private taskService = inject(TaskService);
   tasks = this.taskService.tasks();
+
+  onTaskAction(id: string, action: TaskAction) {
+    switch (action) {
+      case 'complete':
+        this.taskService.completeTask(id);
+        break;
+      case 'cancel':
+        this.taskService.uncompleteTask(id);
+        break;
+      case 'delete':
+        this.taskService.deleteTask(id);
+        break;
+    }
+  }
+
+
+  onSubtaskToggle(id: string, subtaskToggle: { title: string; checked: boolean }) {
+    subtaskToggle.checked ?
+      this.taskService.completeSubtask(id, subtaskToggle.title) :
+      this.taskService.uncompleteSubtask(id, subtaskToggle.title);
+  }
 }
